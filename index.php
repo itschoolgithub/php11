6cfdfb5d97a35af4bd6439cd3521e31e61cfe760
itschoolgithub <itschoolgithub@mail.ru>
2024-11-10 22:12:07 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<p>
+<?php
+    // date_default_timezone_set('Asia/Aqtobe');
+
+    // $a = time(); // 0 1731253638
+    // echo microtime();
+    // $a = microtime(true);
+    // timestamp
+    // 01-01-1970
+    // echo $a;
+    // 2024-10-20 22:00:10
+
+    // for ($i = 0; $i < 1000000000; $i++) {
+    //     $c = $i ** 2;
+    // }
+
+    // sleep(3);
+
+    // $b = time();
+    // $b = microtime(true);
+
+    // echo $b - $a;
+
+    // $a = time();
+
+    // echo date("d.m.Y H:i:s", $a /* + 2 * 60 * 60*/);
+
+    // $date = date("d.m.Y");
+    // $time = date("H:i:s");
+
+    // $t = strtotime("now + 10 months + 1 day - 1 hour - 20 minutes");
+    // $t = strtotime("10.01.2024 12:00:00 - 9 days - 12 hours - 1 second");
+    // echo date("d.m.Y H:i:s", $t);
+?>
+</p>
